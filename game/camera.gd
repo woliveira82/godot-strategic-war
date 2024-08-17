0769d790edcf83176d410201308488aa3758e9d6
Weiner Oliveira <woliveira82@gmail.com>
2024-08-17 23:13:22 +0100

    feat: add camera zoom

--- game/camera.gd
+++ game/camera.gd
@@ -2,11 +2,26 @@ extends Camera2D
 
 
 func _process(delta):
+	process_zoom(delta)
 	simple_pan(delta)
+
+
+func process_zoom(delta):
+	if Input.is_action_pressed("camera_zoom_in"):
+		zoom = zoom.slerp(
+			clamp(zoom * 1.1, Vector2(0.2, 0.2), Vector2(2.0, 2.0)),
+			10.0 * delta,
+		)
+		
+	if Input.is_action_pressed("camera_zoom_out"):
+		zoom = zoom.slerp(
+			clamp(zoom * 0.9, Vector2(0.2, 0.2), Vector2(2.0, 2.0)),
+			10.0 * delta,
+		)
 
 
 func simple_pan(delta):
 	var movement = Vector2.ZERO
 	movement.x = Input.get_axis("camera_move_left", "camera_move_right")
 	movement.y = Input.get_axis("camera_move_up", "camera_move_down")
-	position += movement.normalized() * delta * 1000 * (1/zoom.x)
+	position += movement.normalized() * delta * 1000 * ( 1 / zoom.x )
